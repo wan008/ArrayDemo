6d48113f89012b1d0f100084702f98c9ac67e19b
wan008 <kimw@sheridancollege.ca>
2023-06-17 15:26:49 -0400

    Expend 3 to 10 of Student List

--- src/arrydemo/StudentList.java
+++ src/arrydemo/StudentList.java
@@ -13,11 +13,12 @@ public class StudentList {
         
         Student s1 = new Student("s1", "Kim");
         
-        Student[] studentList = new Student[3];
+        Student[] studentList = new Student[10];
         
         studentList[0] = s1;
         studentList[1] = new Student("s2", "Ronak");
         studentList[2] = new Student("s3", "NewName");
+        studentList[3] = new Student("s4", "NewName3");
         
         for (int i=0;i < studentList.length - 1; i++ ){
             System.out.println(studentList[i].getsName());
