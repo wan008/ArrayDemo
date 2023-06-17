42b668c610b621b175ad24f695c476d8a3647c68
wan008 <kimw@sheridancollege.ca>
2023-06-17 19:29:52 -0400

    Add HalfTimeStudent.java

---
+++ src/arrydemo/HalfTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arrydemo;
+
+/**
+ *
+ * @author bada5
+ */
+public class HalfTimeStudent {
+    int halftime;
+
+    public HalfTimeStudent(int halftime) {
+        this.halftime = halftime;
+    }
+
+    public int getHalftime() {
+        return halftime;
+    }
+
+    public void setHalftime(int halftime) {
+        this.halftime = halftime;
+    }
+    
+    
+    
+}
