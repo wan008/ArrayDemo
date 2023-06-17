04b3efaed19fe993830868b5f58031d3ba6bef41
wan008 <kimw@sheridancollege.ca>
2023-06-17 18:43:12 -0400

    Added PartTimeStudent.java

---
+++ src/arrydemo/PartTimeStudent.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arrydemo;
+
+/**
+ *
+ * @author bada5
+ */
+public class PartTimeStudent {
+    
+    int course;
+
+    public PartTimeStudent(int course) {
+        this.course = course;
+    }
+    
+    
+}
